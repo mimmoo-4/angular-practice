7dc81cb2bb1db360af9ba14209f8308ad5b99ea7
mimmoo-4 <mimmoo-4@users.noreply.github.com>
2025-12-15 17:58:06 +0900

    ルーティングのコメント修正

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,13 @@
 import { Routes } from '@angular/router';
 
-// 1. 新しい配置場所から SignInComponent をインポートします
+// 1. 新しい配置場所から SignIn をインポート
 import { SignIn } from './components/sign-in/sign-in';
 
-// 2. routes 配列に設定を追加します
+// 2. routes 配列に設定を追加
 export const routes: Routes = [
   // '/sign-in' というURLでアクセスされたら SignInComponent を表示する
   { path: 'sign-in', component: SignIn },
 
   // アプリケーションのルートURL ('/') でアクセスされたら '/sign-in' にリダイレクトする
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-
-  // 他のパスの設定が必要であればここに追加します
-  // 例：{ path: 'dashboard', component: DashboardComponent }
 ];
